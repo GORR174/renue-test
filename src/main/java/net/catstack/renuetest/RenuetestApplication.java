220f260bea284e49c7e4a6b44017d13426d85e03
Gorr <gorrgorrob@gmail.com>
2021-03-01 20:13:53 +0500

    Initial commit

---
+++ src/main/java/net/catstack/renuetest/RenuetestApplication.java
@@ -0,0 +1,17 @@
+package net.catstack.renuetest;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RenuetestApplication implements CommandLineRunner {
+	public static void main(String[] args) {
+		SpringApplication.run(RenuetestApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) {
+
+	}
+}
---
+++ src/test/java/net/catstack/renuetest/RenuetestApplicationTests.java
@@ -0,0 +1,13 @@
+package net.catstack.renuetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RenuetestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
