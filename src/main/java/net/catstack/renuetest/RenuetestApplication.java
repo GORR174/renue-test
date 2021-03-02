297c884be2a8cc19433013ea2d2643bc45b65f0e
Gorr <gorrgorrob@gmail.com>
2021-03-02 18:59:03 +0500

    Get column by application.yml or args

--- src/main/java/net/catstack/renuetest/RenuetestApplication.java
+++ src/main/java/net/catstack/renuetest/RenuetestApplication.java
@@ -2,6 +2,7 @@ package net.catstack.renuetest;
 
 import net.catstack.renuetest.controllers.AirportController;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,12 +13,28 @@ public class RenuetestApplication implements CommandLineRunner {
 	@Autowired
 	private AirportController airportController;
 
+	@Value("${app.column}")
+	private int column;
+
 	public static void main(String[] args) {
 		SpringApplication.run(RenuetestApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) {
-		System.out.println(airportController.findAirportsByFieldValue(1, "T"));
+		if (args.length > 0) {
+			try {
+				column = Integer.parseInt(args[0]);
+			} catch (NumberFormatException e) {
+				System.out.println("Неверный формат ввода параметра колонки. Используется стандартный параметр: " + column);
+			}
+		}
+		System.out.println(column);
+
+		try {
+			System.out.println(airportController.findAirportsByFieldValue(column, "T"));
+		} catch (IndexOutOfBoundsException e) {
+			System.out.println("Ошибка: Указанной колонки (" + column + ") не существует");
+		}
 	}
 }
