6aee0932022b927dcb946bfa087fd775fd1c8c40
Gorr <gorrgorrob@gmail.com>
2021-03-03 18:57:18 +0500

    Default tabs refactoring

--- src/main/java/net/catstack/renuetest/RenuetestApplication.java
+++ src/main/java/net/catstack/renuetest/RenuetestApplication.java
@@ -10,31 +10,31 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RenuetestApplication implements CommandLineRunner {
 
-	@Autowired
-	private AirportController airportController;
+    @Autowired
+    private AirportController airportController;
 
-	@Value("${app.column}")
-	private int column;
+    @Value("${app.column}")
+    private int column;
 
-	public static void main(String[] args) {
-		SpringApplication.run(RenuetestApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RenuetestApplication.class, args);
+    }
 
-	@Override
-	public void run(String... args) {
-		if (args.length > 0) {
-			try {
-				column = Integer.parseInt(args[0]);
-			} catch (NumberFormatException e) {
-				System.out.println("Неверный формат ввода параметра колонки. Используется стандартный параметр: " + column);
-			}
-		}
-		System.out.println(column);
+    @Override
+    public void run(String... args) {
+        if (args.length > 0) {
+            try {
+                column = Integer.parseInt(args[0]);
+            } catch (NumberFormatException e) {
+                System.out.println("Неверный формат ввода параметра колонки. Используется стандартный параметр: " + column);
+            }
+        }
+        System.out.println(column);
 
-		try {
-			System.out.println(airportController.findAirportsByFieldValue(column, "T"));
-		} catch (IndexOutOfBoundsException e) {
-			System.out.println("Ошибка: Указанной колонки (" + column + ") не существует");
-		}
-	}
+        try {
+            System.out.println(airportController.findAirportsByFieldValue(column, "T"));
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("Ошибка: Указанной колонки (" + column + ") не существует");
+        }
+    }
 }
